e21f4f7bc33c9c24fef0437ca7b563a7d710ab12
Adam Baldwin <baldwin@andyet.net>
2014-11-04 20:28:58 -0800

    adding more tests for this broken ass code

--- test/index.js
+++ test/index.js
@@ -11,3 +11,29 @@ exports['returns the right dependencies'] = function(test){
         test.done();
     });
 };
+
+exports['properly error on unknown module'] = function (test) {
+    var module = {
+        name: 'helmet8675309',
+        version: '0.2.0'
+    };
+
+    rsnpm.getAllDependencies(module, function (err, results) {
+        test.equals(0, results.length, 'should have 0 results');
+        test.done();
+    });
+};
+
+
+exports['properly error on unknown module of known module'] = function (test) {
+    var module = {
+        name: 'herpmcderp',
+        version: '0.0.6'
+    };
+
+    rsnpm.getAllDependencies(module, function (err, results) {
+        //console.log(results)
+        test.equals(1, results.length, 'should have 0 results');
+        test.done();
+    });
+};
